f5c61c29656dae69cb189f5e106735abc72e8f14
JonathanAndrade19 <JonathanAndrade19>
2020-01-05 13:15:29 -0300

    concluindo a Class ControleRemoto.php todas as funcionalidades e tratando erros.

--- ControleRemoto.php
+++ ControleRemoto.php
@@ -1,42 +1,42 @@
 <?php 
-require_once 'Controlador.php'
+require_once 'Controlador.php';
 class ControleRemoto implements Controlador {
     private $volume;
     private $ligado;
     private $tocando;
 
-    function __construct()
+    public function __construct()
     {
         $this->volume = 50;
         $this->ligado = false;
         $this->tocando = false;
     }
     
-    function getVolume()
+    private function getVolume()
     {
         return $this->volume;
     }
 
-    function setVolume($volume)
+    private function setVolume($volume)
     {
         $this->volume = $volume;
     }
 
-    function getLigado()
+    private function getLigado()
     {
         return $this->ligado;
     }
 
-    function setLigado($ligado)
+    private function setLigado($ligado)
     {
         $this->ligado = $ligado;
     }
 
-    function getTocando()
+    private function getTocando()
     {
         return $this->tocando;
     }
-    function setTocando($tocando)
+    private function setTocando($tocando)
     {
         $this->tocando = $tocando;
     }
@@ -54,10 +54,11 @@ class ControleRemoto implements Controlador {
 
     public function abrirMenu()
     {
-        echo "<br> Está Ligado?: ". ($this->getligado() ? "SIM":"NÃO"); 
-        echo "<br> Está tocando?: ". ($this->getTocando()? "SIM":"NÃO");
+        echo "----------  Menu  ----------";
+        echo "<br> Está Ligado? ". ($this->getligado() ? "SIM":"NÃO"); 
+        echo "<br> Está tocando? ". ($this->getTocando()? "SIM":"NÃO");
         echo "<br> Volume: ". $this->getVolume();
-        for($i = 0; $i <= $this->getVolume(); $i += 10){
+        for($i=0; $i <= $this->getVolume(); $i+=10){
             echo " | ";
         }
         echo "<br>";
@@ -70,32 +71,44 @@ class ControleRemoto implements Controlador {
 
     public function maisVolume()
     {
-        
+        if($this->getLigado()){
+            $this->setLigado($this->getVolume() + 5);
+        }
     }
 
     public function menosVolume()
     {
-        
+        if($this->getLigado()){
+            $this->setLigado($this->getVolume() - 5);
+        }
     }
 
     public function ligarMudo()
     {
-
+        if($this->getLigado() && $this->getVolume() > 0){
+            $this->setVolume(0);
+        }
     }
 
     public function desligarMudo()
     {
-        
+        if($this->getLigado() && $this->getVolume() == 0){
+            $this->setVolume(50);
+        }
     }
 
     public function play()
     {
-        
+        if($this->getLigado() && !($this->getTocando())){
+            $this->setTocando(true);
+        }
     }
 
     public function pause()
     {
-        
+        if($this->getLigado() && $this->getTocando()){
+            $this->setTocando(false);
+        }
     }
 
 }

0977e6e615ab663ff49f234687e022a263c5686e
JonathanAndrade19 <JonathanAndrade19>
2020-01-05 13:19:06 -0300

    Finalizando o mini projeto controle remoto.

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <title>Controle Rmeoto</title>
 </head>
 <body>
-    
+    <h1>Projeto Controle Remoto</h1>
+    <pre>
+        <?php
+            require_once 'ControleRemoto.php';
+            $controle = new ControleRemoto();
+            $controle->ligar();
+            $controle->abrirMenu();
+            
+            
+        ?>
+    </pre>
 </body>
 </html>
